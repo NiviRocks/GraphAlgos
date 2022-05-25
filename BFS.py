6e5fae4a17887ae09ee9141af0676c5b08c3a05f
Nibedita Chakraborty <96379756+NiviRocks@users.noreply.github.com>
2022-05-25 22:27:54 +0530

    used adjacency list using dictionary

--- BFS.py
+++ BFS.py
@@ -7,25 +7,17 @@ class Graph:
     # front and rear for accessing queue
     front=0
     rear=0
-    # stores Adjecency List as dictionary
-    Adj_Dict={}
-    def graph(Adj_Mtx): #converts Adj_MTX into Adj_Dict
-        for i in Adj_Mtx:
-            if i[0] not in Graph.Adj_Dict:
-                Graph.Adj_Dict[i[0]]=[i[1]]
-            else:
-                Graph.Adj_Dict[i[0]].append(i[1])
-    def ShowGraph(): #displays graph
-        for i in Graph.Adj_Dict:
-            print(i,"->",Graph.Adj_Dict[i])
-    def Display_BFS(curr): # displays BFS sequence
+    def ShowGraph(Adj_Dict): #displays graph
+        for i in Adj_Dict:
+            print(i,"->",Adj_Dict[i])
+    def Display_BFS(curr,Adj_Dict): # displays BFS sequence
         print(curr,end=" ")
-        if curr in Graph.Adj_Dict :
+        if curr in Adj_Dict :
             if curr not in Graph.visited: 
                 Graph.visited.append(curr) 
                 Graph.queue.append(curr) 
                 Graph.rear+=1
-            for i in Graph.Adj_Dict[curr]: # iterate over all neighbours of curr
+            for i in Adj_Dict[curr]: # iterate over all neighbours of curr
                 if i not in Graph.visited: 
                     Graph.queue.append(i)
                     Graph.visited.append(i)
@@ -35,17 +27,16 @@ class Graph:
         if Graph.front==Graph.rear: # no new node to visit
             return
         else:
-            Graph.Display_BFS(Graph.queue[Graph.front]) # go to next node
+            Graph.Display_BFS(Graph.queue[Graph.front],Adj_Dict) # go to next node
         return
 
 #__main__
 #Dry Run
-g=[[1,2],[1,4],[2,4],[2,5],[4,7],[4,5],[5,3],[5,1],[5,6],[6,8],[6,3],[8,7]] # this is directed graph
+#g is an adjecency list in form of dictionary
+g={1:[2,4],2:[4,5],4:[7,5],5:[1,3,6],6:[3,8],8:[7]} # this is directed graph
 # for undirected graph each edge has to be given twice
-# Eg:- edge from 1-2 input as [[1,2],[2,1]]
-Graph.graph(g) #create graph
-print("Display Graph")
-Graph.ShowGraph()
+# Eg:- edge from 1-2 input as {1:[2]}
+Graph.ShowGraph(g)
 print("BFS Sequence")
-Graph.Display_BFS(1) #passing start node
+Graph.Display_BFS(1,g) #passing start node
           
